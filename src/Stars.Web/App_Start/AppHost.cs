41e43a26ba11c501472572221a0ff4806506ba5e
John Fair <johnfair@quickenloans.com>
2013-09-03 15:55:21 -0400

    Fixed broken ServiceStack setup, wired up StructureMap IOC properly w/ Funq

--- src/Stars.Web/App_Start/AppHost.cs
+++ src/Stars.Web/App_Start/AppHost.cs
@@ -1,23 +1,33 @@
-﻿using ServiceStack.WebHost.Endpoints;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Funq;
+using ServiceStack.WebHost.Endpoints;
 
-namespace Stars.Web
+using Stars.Web.Api;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(Stars.Web.App_Start.AppHost), "Start")]
+namespace Stars.Web.App_Start
 {
     public class AppHost : AppHostBase
-    {
+	{		
+		public AppHost() //Tell ServiceStack the name and where to find your web services
+			: base("STARS API", typeof(ClientService).Assembly) { }
 
-        public AppHost() : base("Stars", typeof(Stars.Web.Api.ClientService).Assembly)
-        {
+		public override void Configure(Funq.Container container)
+		{
+			//Set JSON web services to return idiomatic JSON camelCase properties
+			ServiceStack.Text.JsConfig.EmitCamelCaseNames = true;
 
-        }
+            //Setup StructureMap as the IOC to use instead of Funq
+            container.Adapter = new StructureMapContainerAdapter();
 
-        public override void Configure(Container container)
-        {
-            
-        }
-    }
+            //TODO: Register your dependencies
+            //ObjectFactory.Inject(typeof(IFoo), new Foo());
+
+			//Set MVC to use the same Funq IOC as ServiceStack
+			//ControllerBuilder.Current.SetControllerFactory(new FunqControllerFactory(container));
+		}
+
+		public static void Start()
+		{
+			new AppHost().Init();
+		}
+	}
 }
