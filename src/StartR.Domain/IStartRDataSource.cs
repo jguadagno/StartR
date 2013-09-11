c0d4665a124f6844397d33eca8c856fb405d8a4f
Keith Elder <keith@keithelder.net>
2013-09-11 08:45:07 -0400

    refactoring name to StartR

---
+++ src/StartR.Domain/IStartRDataSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartR.Domain
+{
+    public interface IStartRDataSource
+    {
+        IQueryable<Client> Clients { get;  }
+        IQueryable<Loan> Loans { get;  }
+        IQueryable<Lead> Leads { get; }
+    }
+}
